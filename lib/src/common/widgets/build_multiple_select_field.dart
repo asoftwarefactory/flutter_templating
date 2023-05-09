f1854765a39da41b97c64b1ab399e45a81905dea
mario <mario.mazzarelli@asfweb.net>
2023-05-09 16:23:13 +0200

    ..

--- lib/src/common/widgets/build_multiple_select_field.dart
+++ lib/src/common/widgets/build_multiple_select_field.dart
@@ -1,8 +1,8 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 import 'package:flutter_templating/flutter_templating.dart';
-import '../utils/unique_value.dart';
 import 'inputs/multiselect_dropdown_field.dart';
 
 class BuildMultipleSelectField extends ConsumerWidget {
@@ -28,41 +28,67 @@ class BuildMultipleSelectField extends ConsumerWidget {
           () => FormControl<List<String>>(value: defaultValue, touched: true),
           validators: section.validators,
         ) as FormControl<List<String>>;
-        return MultiSelectDropdownField(control: control, section: section);
+        return MultiSelectDropdownField(
+          control: control,
+          section: section,
+          label: (v, items) =>
+              items.firstWhereOrNull((e) => e.key as String == v)?.label ?? "",
+        );
       case FieldTypes.Integer:
         final control = form.getOrSetAbstractControlAndSetValidators(
           section.id!,
           () => FormControl<List<int>>(value: defaultValue, touched: true),
           validators: section.validators,
         ) as FormControl<List<int>>;
-        return MultiSelectDropdownField(control: control, section: section);
+        return MultiSelectDropdownField(
+          control: control,
+          section: section,
+          label: (v, items) =>
+              items.firstWhereOrNull((e) => e.key as int == v)?.label ?? "",
+        );
       case FieldTypes.Decimal:
         final control = form.getOrSetAbstractControlAndSetValidators(
           section.id!,
           () => FormControl<List<double>>(value: defaultValue, touched: true),
           validators: section.validators,
         ) as FormControl<List<double>>;
-        return MultiSelectDropdownField(control: control, section: section);
+        return MultiSelectDropdownField(
+          control: control,
+          section: section,
+          label: (v, items) =>
+              items.firstWhereOrNull((e) => e.key as double == v)?.label ?? "",
+        );
       case FieldTypes.Currency:
         final control = form.getOrSetAbstractControlAndSetValidators(
           section.id!,
           () => FormControl<List<double>>(value: defaultValue, touched: true),
           validators: section.validators,
         ) as FormControl<List<double>>;
-        return MultiSelectDropdownField(control: control, section: section);
+        return MultiSelectDropdownField<double>(
+          control: control,
+          section: section,
+          label: (v, items) =>
+              items.firstWhereOrNull((e) => e.key as double == v)?.label ?? "",
+        );
       case FieldTypes.Boolean:
         final control = form.getOrSetAbstractControlAndSetValidators(
           section.id!,
-          () => FormControl<List<Unique<bool>>>(
-              value: defaultValue, touched: true),
+          () => FormControl<List<String>>(value: defaultValue, touched: true),
           validators: section.validators,
-        ) as FormControl<List<Unique<bool>>>;
-        return MultiSelectDropdownField(
+        ) as FormControl<List<String>>;
+        final mappedItems = (section.items ?? [])
+            .asMap()
+            .map(
+                (k, v) => MapEntry(k, Item(key: "${v.key}_$k", label: v.label)))
+            .values
+            .toList();
+        final sec = section.copyWith(items: mappedItems);
+        return MultiSelectDropdownField<String>(
           control: control,
-          section: section,
-          valueFromSectionItem: (item) => Unique(value: item.key as bool),
+          section: sec,
+          label: (v, items) =>
+              items.firstWhere((i) => v == i.key.toString()).label ?? '',
         );
-
       // DATES -----------------------------------------------------
       case FieldTypes.DateUtc:
         final control = form.getOrSetAbstractControlAndSetValidators(
@@ -70,28 +96,48 @@ class BuildMultipleSelectField extends ConsumerWidget {
           () => FormControl<List<String>>(value: defaultValue, touched: true),
           validators: section.validators,
         ) as FormControl<List<String>>;
-        return MultiSelectDropdownField(control: control, section: section);
+        return MultiSelectDropdownField(
+          control: control,
+          section: section,
+          label: (v, items) =>
+              items.firstWhereOrNull((e) => e.key as String == v)?.label ?? "",
+        );
       case FieldTypes.DateNoUtc:
         final control = form.getOrSetAbstractControlAndSetValidators(
           section.id!,
           () => FormControl<List<String>>(value: defaultValue, touched: true),
           validators: section.validators,
         ) as FormControl<List<String>>;
-        return MultiSelectDropdownField(control: control, section: section);
+        return MultiSelectDropdownField(
+          control: control,
+          section: section,
+          label: (v, items) =>
+              items.firstWhereOrNull((e) => e.key as String == v)?.label ?? "",
+        );
       case FieldTypes.DateTime:
         final control = form.getOrSetAbstractControlAndSetValidators(
           section.id!,
           () => FormControl<List<String>>(value: defaultValue, touched: true),
           validators: section.validators,
         ) as FormControl<List<String>>;
-        return MultiSelectDropdownField(control: control, section: section);
+        return MultiSelectDropdownField(
+          control: control,
+          section: section,
+          label: (v, items) =>
+              items.firstWhereOrNull((e) => e.key as String == v)?.label ?? "",
+        );
       case FieldTypes.Time:
         final control = form.getOrSetAbstractControlAndSetValidators(
           section.id!,
           () => FormControl<List<String>>(value: defaultValue, touched: true),
           validators: section.validators,
         ) as FormControl<List<String>>;
-        return MultiSelectDropdownField(control: control, section: section);
+        return MultiSelectDropdownField(
+          control: control,
+          section: section,
+          label: (v, items) =>
+              items.firstWhereOrNull((e) => e.key as String == v)?.label ?? "",
+        );
       // DATES ----------------------------------------------------- END
       case FieldTypes.File:
         final control = form.getOrSetAbstractControlAndSetValidators(
@@ -99,7 +145,12 @@ class BuildMultipleSelectField extends ConsumerWidget {
           () => FormControl<List<String>>(value: defaultValue, touched: true),
           validators: section.validators,
         ) as FormControl<List<String>>;
-        return MultiSelectDropdownField(control: control, section: section);
+        return MultiSelectDropdownField(
+          control: control,
+          section: section,
+          label: (v, items) =>
+              items.firstWhereOrNull((e) => e.key as String == v)?.label ?? "",
+        );
       default:
         return const SizedBox();
     }
--- lib/src/common/widgets/inputs/multiselect_dropdown_field.dart
+++ lib/src/common/widgets/inputs/multiselect_dropdown_field.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reactive_dropdown_multiple_field/reactive_dropdown_multiple_field.dart';
@@ -10,16 +9,25 @@ import 'input_field.dart';
 class MultiSelectDropdownField<T> extends ConsumerWidget implements InputField {
   final FormControl<List<T>> control;
   final Section section;
-  final T Function(Item)? valueFromSectionItem;
+  final String Function(T data, List<Item> allItems)? label;
 
   const MultiSelectDropdownField({
     super.key,
     required this.control,
     required this.section,
-    this.valueFromSectionItem,
+    this.label,
   });
+
   bool get _readOnly {
     return section.readonly == true;
+  }
+
+  List<Item> get _sectionItems {
+    return section.items ?? [];
+  }
+
+  List<T> get _typedSectionItems {
+    return (section.items ?? []).map((e) => e.key as T).toList();
   }
 
   @override
@@ -28,18 +36,18 @@ class MultiSelectDropdownField<T> extends ConsumerWidget implements InputField {
       ignoring: _readOnly,
       child: Opacity(
         opacity: _readOnly ? 0.5 : 1,
-        child: ReactiveDropdownMultipleField<T, T>(
+        child: ReactiveDropdownMultipleField<T>(
           readOnly: _readOnly,
           control: control,
           whenEmpty: section.names?.getDescriptionLabelTranslated(context),
-          labelBuilder: (options, value) {
-            final item =
-                (section.items ?? []).firstWhereOrNull((e) => e.key == value);
-            return item?.label ?? '';
-          }, 
-          options: (section.items ?? [])
-              .map((e) => valueFromSectionItem?.call(e) ?? e.key as T)
-              .toList(),
+          options: _typedSectionItems,
+          labelBuilder: (v) {
+            return LabelBuilderData(
+              enableIntoSelectedOptions: true,
+              enableIntoOptions: true,
+              label: label?.call(v, _sectionItems) ?? v.toString(),
+            );
+          },
         ),
       ),
     );
